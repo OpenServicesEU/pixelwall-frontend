cf66daa1e84ada5f3cc659aad8ff2d0341bf74af
Michael Fladischer <FladischerMichael@fladi.at>
2014-09-08 12:08:55 +0200

    Rename to new project title.

--- pixelwall.py
+++ pixelwall.py
@@ -59,12 +59,12 @@ class Page(QWebPage):
         super(Page, self).__init__(parent)
 
     def userAgentForUrl(self, url):
-        return "Evitor Public Display Client"
+        return "PixelWall Public Display Client"
 
 class FrontEnd(QWidget):
     def __init__(self, url, tty, parent=None):
         super(FrontEnd, self).__init__(parent)
-        self.setWindowTitle('Evitor')
+        self.setWindowTitle('PixelWall')
         self.page = Page()
         self.page.mainFrame().loadFinished.connect(self.loadFinished)
         self.page.mainFrame().javaScriptWindowObjectCleared.connect(self.javaScriptWindowObjectCleared)
@@ -87,7 +87,7 @@ class FrontEnd(QWidget):
         self.setAutoFillBackground(True);
 
     def javaScriptWindowObjectCleared(self):
-        self.page.mainFrame().addToJavaScriptWindowObject("Evitor", hub)
+        self.page.mainFrame().addToJavaScriptWindowObject("PixelWall", hub)
         print('javaScriptWindowObjectCleared has finished')
 
     def loadFinished(self):
@@ -96,9 +96,9 @@ class FrontEnd(QWidget):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Simplistic browser frontend for EviTor.')
+    parser = argparse.ArgumentParser(description='Simplistic browser frontend for PixelWall.')
     parser.add_argument('--verbose', action='store_true', help='Be verbose')
-    parser.add_argument('url', type=QUrl, help='The URL for the Evitor master server')
+    parser.add_argument('url', type=QUrl, help='The URL for the PixelWall master server')
     parser.add_argument('tty', type=str, default='/dev/ttymxc3', help='TTY on which the SAM is connected')
 
     args = parser.parse_args()
